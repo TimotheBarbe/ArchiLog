269c46ec458877d4f2bd187d05c8c191d7e6f3bd
TimotheBarbe <timothe.barbe@wanadoo.fr>
2014-11-04 22:53:26 +0100

    Main + Remplir + Sequence

--- ArchiLog/src/Main/Main.java
+++ ArchiLog/src/Main/Main.java
@@ -16,12 +16,16 @@ public class Main {
 		Point[] points = new Point[] { p1, p2, p3 };
 		PolygoneImpl pi = new PolygoneImpl(points);
 
-		Crayon cr = new Crayon();
+		Crayon cr1 = new Crayon();
+		Crayon cr2 = new Crayon(2, Couleur.BLEU);
 
-		Dessiner d1 = new Dessiner(c, cr);
-		d1.run();
+		Dessiner d1 = new Dessiner(c, cr1);
+		Dessiner d2 = new Dessiner(pi, cr1);
 
-		Dessiner d2 = new Dessiner(pi, cr);
-		d2.run();
+		Sequence s1 = new Sequence(d1, d2);
+
+		Remplir r1 = new Remplir(pi, cr2);
+		Sequence s2 = new Sequence(s1, r1);
+		s2.run();
 	}
 }
--- ArchiLog/src/langage/operateurs/Sequence.java
+++ ArchiLog/src/langage/operateurs/Sequence.java
@@ -1,12 +1,18 @@
 package langage.operateurs;
 
 public class Sequence implements OperateursInterface {
+
 	private OperateursInterface operation1;
 	private OperateursInterface operation2;
 
+	public Sequence(OperateursInterface operation1,
+			OperateursInterface operation2) {
+		this.operation1 = operation1;
+		this.operation2 = operation2;
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		operation1.run();
 		operation2.run();
 	}
