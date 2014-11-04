d6bcbdb3035bff30c70bee57b06f58c48ca36b03
TimotheBarbe <timothe.barbe@wanadoo.fr>
2014-11-04 21:30:41 +0100

    Add Cercle

--- ArchiLog/src/langage/type/PolygoneImpl.java
+++ ArchiLog/src/langage/type/PolygoneImpl.java
@@ -1,5 +1,27 @@
 package langage.type;
 
+import java.awt.Point;
+import java.util.Arrays;
+
 public class PolygoneImpl implements CheminFerme {
+
+	private Point points[];
+
+	public PolygoneImpl(Point[] points) {
+		this.points = points;
+	}
+
+	public Point[] getPoints() {
+		return points;
+	}
+
+	public void setPoints(Point[] points) {
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "PolygoneImpl [points=" + Arrays.toString(points) + "]";
+	}
 
 }
