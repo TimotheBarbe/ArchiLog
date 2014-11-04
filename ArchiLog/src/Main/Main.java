dfe10cf72fab7dca7e681f0b5912f84063123d8b
TimotheBarbe <timothe.barbe@wanadoo.fr>
2014-11-04 22:29:08 +0100

    main

--- ArchiLog/src/Main/Main.java
+++ ArchiLog/src/Main/Main.java
@@ -1,8 +1,19 @@
 package Main;
 
+import java.awt.Point;
+
+import langage.type.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Point p1 = new Point(50, 50);
+		Cercle c = new Cercle(p1, 20);
+
+		Point p2 = new Point(50, 30);
+		Point p3 = new Point(20, 30);
+		Point[] points = new Point[] { p1, p2, p3 };
+		PolygoneImpl pi = new PolygoneImpl(points);
 		
 	}
 
