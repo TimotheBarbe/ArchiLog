c6eb624125aa44b61302a6dae87867eddbbe207f
TimotheBarbe <timothe.barbe@wanadoo.fr>
2014-11-05 22:32:20 +0100

    Implémentation de BezierImpl

--- ArchiLog/src/langage/type/BezierImpl.java
+++ ArchiLog/src/langage/type/BezierImpl.java
@@ -1,5 +1,24 @@
 package langage.type;
 
+import java.awt.Point;
+
 public class BezierImpl implements CheminOuvert {
 
+	private Point points[];
+
+	public BezierImpl(Point point1, Point point2) {
+		this.points = new Point[] { point1, point2 };
+	}
+
+	public BezierImpl(Point point1, Point point2, Point point3) {
+		this.points = new Point[] { point1, point2, point3 };
+	}
+
+	public Point[] getPoints() {
+		return points;
+	}
+
+	public void setPoints(Point[] points) {
+		this.points = points;
+	}
 }
