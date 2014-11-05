45c90ce34dbdc1351a494f20116e2df015bc7cfc
TimotheBarbe <timothe.barbe@wanadoo.fr>
2014-11-05 20:48:11 +0100

    modif interpreteur

--- ArchiLog/src/interpretation/InterpreteurInterface.java
+++ ArchiLog/src/interpretation/InterpreteurInterface.java
@@ -1,20 +1,13 @@
 package interpretation;
 
-import langage.operateurs.OperateursInterface;
-import langage.type.Cercle;
 import langage.type.Chemin;
+import langage.type.CheminFerme;
 import langage.type.Crayon;
-import langage.type.PolygoneImpl;
 
 public interface InterpreteurInterface {
-	public void dessinCercle(Cercle c, Crayon cr);
 
 	public void dessiner(Chemin c, Crayon cr);
 
-	public void dessinPolygone(PolygoneImpl p, Crayon cr);
-
-	public void remplissageCercle(Cercle c, Crayon cr);
-
-	public void remplissagePolygone(PolygoneImpl p, Crayon cr);
+	public void remplissage(CheminFerme c, Crayon cr);
 
 }
